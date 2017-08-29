5d1c550f16a1df246dc79a83f3df39efde611eba
Lorena Silveira <lima.lory@gmail.com>
2017-08-29 00:42:55 -0300

    organizando a busca gulosa no onScannedRobot

--- Teste.java
+++ Teste.java
@@ -1,11 +1,8 @@
-package TrabalhoIA;
+package Trab_IA;
 
 import static robocode.util.Utils.normalRelativeAngleDegrees;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
-
+import java.util.ArrayList;
 import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
 import robocode.WinEvent;
@@ -14,9 +11,12 @@ public class Teste extends AdvancedRobot {
 
 	int contador = 0;
 	double posicaoDeTiro; // posicao onde se deve atirar ou mover o robo
-	Random random = new Random();
-	Map<String, Double> listaRobosAchados = new HashMap<String, Double>();
-
+	Random random = new Random(); 
+	ArrayList< ScannedRobotEvent > listaRobosAchados = new ArrayList< ScannedRobotEvent >();
+	ScannedRobotEvent roboMenorDistancia;
+	ScannedRobotEvent roboAtual;
+	int i = 0;
+	
 	public void run() {
 
 		posicaoDeTiro = 10; // Inicializa posicaoDeTiro com angulo de 10
@@ -28,38 +28,43 @@ public class Teste extends AdvancedRobot {
 			}
 			
 			execute();
-
+			
 		}
-
 	}
-
 	// quando achar um robo adversario
 	public void onScannedRobot(ScannedRobotEvent e) {
 		
-		listaRobosAchados.put(e.getName(), e.getDistance());
+		listaRobosAchados.add(e);
 		
-		for (String key : listaRobosAchados.keySet()) {
-            
-            Double value = listaRobosAchados.get(key);
-            
-            System.out.println(key + " = " + value);
+		roboMenorDistancia = listaRobosAchados.get(i); // roboAlvo recebe o primeiro robo da lista, ele será o a escolha de menor custo da busca gulosa
+		
+		for(ScannedRobotEvent robo  : listaRobosAchados){
+			  System.out.println(i + " - " + listaRobosAchados.get(i));
+			  roboAtual = listaRobosAchados.get(i); // guarda o roboAtual do for
+			  
+			  if (roboMenorDistancia.getDistance() > roboAtual.getDistance() ){  // compara a energia do robô roboAlvo(anterior) com o roboAtual
+				  roboMenorDistancia = roboAtual;
+			  }
 		}
 		
 		execute();
 		
-
-		caminhar(e);
-
-		atirar(e);
+		atirar(roboMenorDistancia);
+		
+		if (e.getDistance() < 100) { // se a distanca for menor que 100 ele tá mto proximo
+			if (e.getBearing() > -90 && e.getBearing() <= 90) {  // se ele estver do lado esquerdo
+				back(40); // volta distancia de 40 para tras
+			} else {
+				ahead(40); // caminha distancia de 40 para frente
+			}
+		}
+		
+		
 		
 		
 		
 	}
 	
-	public void caminhar(ScannedRobotEvent e) {
-	
-	}
-
 	public void atirar(ScannedRobotEvent e) {
 		
 
